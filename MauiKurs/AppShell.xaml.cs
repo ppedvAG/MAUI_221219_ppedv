505e4b222495473e5578ef99bbb4381b45c9655b
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-12-20 09:53:30 +0100

    Modul 3: Grundlagen

--- MauiKurs/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MauiKurs;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
---
+++ MauiKurs/M03_Xaml-Grundlagen.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace MauiKurs;
+
+public partial class XamlGrundlagen : ContentPage
+{
+	public XamlGrundlagen()
+	{
+		InitializeComponent();
+	}
+
+    private async void Btn_KlickMich_Clicked(object sender, EventArgs e)
+    {
+        (sender as Button).Text = "Button wurde geklickt";
+
+        //Mittels eines DisplayAlerts können kleine (asynkrone) Aussagen und Abfragen an den Benutzer ausgegeben
+        if (await DisplayAlert("FRAGE", "Soll was gemacht werden?", "Ja", "Nein"))
+        //Verändern der Text-Eigenschaft (angezeigter Text) des Buttons
+        {
+            (sender as Button).Text = "Button wurde geklickt";
+            Lbl_Output.Text = "Button wurde geklickt";
+        }
+    }
+
+}
+
